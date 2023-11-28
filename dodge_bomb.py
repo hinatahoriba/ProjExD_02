1b94fd61b7be34f0f152996221ffbeda6894ef85
hinata <c0a2212903@edu.teu.ac.jp>
2023-11-28 16:19:00 +0900

    演習2時間と共に爆弾が大きくなる

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,29 +1,25 @@
-import sys
 import random
+import sys
 import pygame as pg
-
-
 WIDTH, HEIGHT = 1600, 900
-delta = {
-    pg.K_UP: (0, -5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5,0),
-    pg.K_RIGHT:(+5,0)
+delta = {  # 練習３：押下キーと移動量の辞書
+    pg.K_UP: (0, -5),  # キー：移動量／値：（横方向移動量，縦方向移動量）
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
 }
-
-
-def check_bound(rct: pg.Rect) -> tuple[bool, bool]:#型ヒント
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
-    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
-    引数rct:こうかとんor爆弾SurfaceのRect
-    戻り値：横方向、縦方向判定結果（画面内：True/画面外：False）
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数 rct：こうかとんor爆弾SurfaceのRect
+    戻り値：横方向，縦方向はみ出し判定結果（画面内：True／画面外：False）
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向はみ出し判定
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向はみ出し判定
         tate = False
-    return (yoko, tate)
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -31,58 +27,56 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
-    clock = pg.time.Clock()
-    tmr = 0
-    
-    bb_img = pg.Surface((20,20))  #練習1：透明なsurfaceを作る
-    bb_img.set_colorkey((0, 0, 0))  #透過指せている
-    pg.draw.circle(bb_img, (255,0,0),(10,10),10)  #練習1：赤い円を透明な正方形の中心(10,10)に描画
-    bb_rct = bb_img.get_rect()
+    kk_rct = kk_img.get_rect()  # 練習３：こうかとんSurfaceのRectを抽出する
+    kk_rct.center = 900, 400  # 練習３：こうかとんの初期座標
+    #演習2：爆弾を10個作成
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))   # 練習１：透明のSurfaceを作る
+        bb_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+        
+    bb_rct = bb_imgs[0].get_rect()  # 練習１：爆弾SurfaceのRectを抽出する    
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    
-    vx,vy = +5,-5  #練習2：爆弾の速度
-    
-    
-    
+    vx, vy = +5, +5  # 練習２：爆弾の速度
+    clock = pg.time.Clock()
+    tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT:#×ボタンを押されたらリターン　閉じる
+            if event.type == pg.QUIT: 
                 return
-            
+
         if kk_rct.colliderect(bb_rct):
             print("Game Over")
             return
-            
-            
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, tpl in delta.items():
-            if key_lst[k]:  #keyが
+            if key_lst[k]:  # キーが押されたら
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         
-        screen.blit(bg_img, [0, 0])#写真をスクリーンに貼り付ける
-        screen.blit(kk_img, kk_rct)#貼り付ける順番も大事
-        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(bg_img, [0, 0])
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(bb_img, bb_rct)#rctで座標を
-        bb_rct.move_ip(vx,vy)
+        screen.blit(kk_img, kk_rct)  # 練習３：こうかとんを移動させる
+        
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 横方向にはみ出たら
             vx *= -1
         if not tate:  # 縦方向にはみ出たら
             vy *= -1
         bb_rct.move_ip(vx, vy)
-        
-        pg.display.update()#スクリーンを更新する。変更を加えても更新しなければ意味が無い
+        screen.blit(bb_img, bb_rct)
+        pg.display.update()
         tmr += 1
         clock.tick(50)
-
-
 if __name__ == "__main__":
     pg.init()
     main()
